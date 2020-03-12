2c518a05d3faf8931ff70bbe7a7495898c50a944
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-03-12 19:08:51 -0300

    Add files via upload

--- Sensor/sensor.py
+++ Sensor/sensor.py
@@ -1,4 +1,8 @@
 import random
+import time
+
+def tn(h,m,s):
+    return h*3600+m*60+s
 
 def sensor(n):
     data = {}
@@ -9,8 +13,14 @@ def sensor(n):
     data['blood_oxygen'] = round(random.uniform(95,100),n)
     data['heart_rate'] = round(random.randint(60,100),n)
     data['awRR'] = random.randint(12,20)
-
     return data
 
+def timer(h,m,s):
+    t = tn(h,m,s)
+    while True:
+        time.sleep(t)
+        print(sensor(2))
+
+
 if __name__ == '__main__':
-    print(sensor(2))
+    timer(0,0,1)
