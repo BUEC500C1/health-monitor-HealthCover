1d9d0cd710187c2b66b5bacea9f2b685aea1e5d4
lqi25 <55333782+lqi25@users.noreply.github.com>
2020-04-20 23:21:48 -0400

    Update predict.py

--- Prediction/predict.py
+++ Prediction/predict.py
@@ -24,35 +24,51 @@ def prediction(time):
     #dataset = pd.read_csv(r'health.csv')
     if time > 0:
         x = []
-        yPulse = []
-        yBP = []
+        yHR = []
+        yBPL = []
+        yBPH = []
         yBO = []
+        yawRR = []
         xx=dataset.loc[0:dataset['Time'].size-1,'Time'].values
-        Pulse=dataset.loc[0:dataset['Pulse'].size-1,'Pulse'].values
-        BP=dataset.loc[0:dataset['BloodPressure'].size-1,'BloodPressure'].values
+        HR=dataset.loc[0:dataset['HeartRate'].size-1,'HeartRate'].values
+        BPL=dataset.loc[0:dataset['BloodPressureLow'].size-1,'BloodPressureLow'].values
+        BPH = dataset.loc[0:dataset['BloodPressureHigh'].size - 1, 'BloodPressureHigh'].values
         BO=dataset.loc[0:dataset['BloodOxygen'].size-1,'BloodOxygen'].values
+        awRR = dataset.loc[0:dataset['awRR'].size - 1, 'awRR'].values
         for i in range(0,len(xx)):
             if xx[i] < time:
                 x.append(xx[i])
-                yPulse.append(Pulse[i])
-                yBP.append(BP[i])
+                yHR.append(HR[i])
+                yBPL.append(BPL[i])
+                yBPH.append(BPH[i])
                 yBO.append(BO[i])
+                yawRR.append(awRR[i])
         x=np.array([x]).T
-        yPulse=np.array([yPulse]).T
-        yBP=np.array([yBP]).T
+        yHR=np.array([yHR]).T
+        yBPL=np.array([yBPL]).T
+        yBPH = np.array([yBPH]).T
         yBO=np.array([yBO]).T
+        yawRR=np.array([yawRR]).T
         regPulse=LinearRegression()
-        regPulse.fit(x,yPulse)
+        regPulse.fit(x,yHR)
         Pulse_pred=int(regPulse.predict(np.array([[time]])))
-        print("Pulse prediction: ",Pulse_pred)
-        regBP=LinearRegression()
-        regBP.fit(x,yBP)
-        BP_pred=int(regBP.predict(np.array([[time]])))
-        print("Blood pressure prediction: ",BP_pred)
+        print("Heart Rate prediction: ",Pulse_pred)
+        regBPL=LinearRegression()
+        regBPL.fit(x,yBPL)
+        BPL_pred=int(regBPL.predict(np.array([[time]])))
+        print("Blood pressure low prediction: ",BPL_pred)
+        regBPH = LinearRegression()
+        regBPH.fit(x, yBPH)
+        BPH_pred = int(regBPH.predict(np.array([[time]])))
+        print("Blood pressure high prediction: ", BPH_pred)
         regBO=LinearRegression()
         regBO.fit(x,yBO)
         BO_pred=int(regBO.predict(np.array([[time]])))
         print("Blood oxygen prediction: ",BO_pred)
+        regawRR = LinearRegression()
+        regawRR.fit(x, yawRR)
+        awRR_pred = int(regawRR.predict(np.array([[time]])))
+        print("awRR prediction: ", awRR_pred)
         print()
     else:
         print("time should be bigger than 0")
